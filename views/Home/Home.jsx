3bd761724d3a68bb1d2ddf21d0f0fa00aa10c30e
ztothes <zygimantas.stonkus@gmail.com>
2019-12-17 21:12:29 +0200

    3rd party select component

--- views/Home/Home.jsx
+++ views/Home/Home.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
+import Select from 'react-select';
 
 import Header from '../../components/Header';
 
 import './Home.scss';
 
 const Home = () => {
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'Suoka', label: 'Suoka' },
+  ];
+
   return (
     <div className='home'>
       <Header title="Suoka">
-        <input type="text" />
+        <Select options={options} />
       </Header>
     </div>
   );
 }
+
+
 
 export default Home;
