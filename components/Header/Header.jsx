f358db4d764a1ecc9d90f999dc22c5d13138070a
ztothes <zygimantas.stonkus@gmail.com>
2019-12-17 20:56:03 +0200

    Added header component

---
+++ components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'React';
+
+import './Header.scss';
+
+const Header = () => {
+  return(
+    <header className='header'>
+      <h1 className='header__title'>Suoka</h1>;
+    </header>
+  );
+}
+
+
+
+export default Header;
